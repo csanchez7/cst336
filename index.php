5f740ff07cc2d181230cfcedf91f3cb551ca4c66
Carlos Sanchez <casanchez@csumb.edu>
2018-06-17 00:07:30 +0000

    starting over bad push

---
+++ index.php
@@ -0,0 +1,18 @@
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo "Carlos Sanchez CST336 Internet Programming";
+
+?>
+
+    <br  /> <br  />
+
+    <a href= "labs/lab1/" > Lab 1 </a>
+  
+
+
+</body>
+</html>
