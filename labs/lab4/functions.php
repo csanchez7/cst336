d64d5e3f6a975a837c1b6df866dec7847144fb7d
Carlos Sanchez <casanchez@csumb.edu>
2018-07-05 23:06:28 +0000

    starting lesson 2

--- labs/lab4/functions.php
+++ labs/lab4/functions.php
@@ -16,6 +16,13 @@
             echo "<td><img src ='$itemImage'></td>";
             echo "<td><h4>$itemName</h4></td>";
             echo "<td><h4>$itemPrice</h4></td>";
+            
+            //Hidden input element containing the item name
+            echo "<form method='post'>";
+            echo "<input type='hidden' name='itemName' value='$itemName'>";
+            echo "<td><button class = 'btn btn-warning'>Add</button></td>";
+            echo "</form>";
+            
             echo "</tr>";
          }
          echo "</table>";
