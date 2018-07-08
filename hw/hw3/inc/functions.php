494fd6fec1a82fffdcd011a0bf677f4caa9619f7
Carlos Sanchez <casanchez@csumb.edu>
2018-07-08 06:34:01 +0000

    hw3 complete

--- hw/hw3/inc/functions.php
+++ hw/hw3/inc/functions.php
@@ -1,49 +1,139 @@
 <?php
-
+    
     $cartoons = array("Captain Planet","Bob's Burgers","Ninja Turtles","PowerPuff Girls",
                         "Batman TAS", "Adventure Time", "Daria","Rick and Morty", "King of the Hill",
                         "Sailer Moon", "Dragon Ball Z","The New Adventures of Batman", "G.I. Joe",
-                        "Looney Tunes","The Flintstones","She-Ra");
-
-    $campy = array("The New Adventures of Batman","Ninja Turtles","G.I. Joe",
-                    "Sailer Moon","Looney Tunes","The Flintstones","She-Ra");
-    $action = array("Captain Planet","Batman TAS","PowerPuff Girls","Dragon Ball Z","She-Ra");
-    $comedy = array("Rick and Morty", "Daria","Bob's Burgers","Ninja Turtles",
-                    "Adventure Time","King of the Hill","Looney Tunes","The Flintstones");
-    $psa = array("G.I. Joe", "Captain Planet","Looney Tunes","She-Ra");
+                        "Looney Tunes","The Flintstones","She-Ra");    
     
-    $Boys = array("The New Adventures of Batman","Ninja Turtles","G.I. Joe","Looney Tunes","Batman TAS",
-                    "Captain Planet","Dragon Ball Z", "Adventure Time");
-    $Girls = array("Sailer Moon","Looney Tunes","PowerPuff Girls", "Daria", "Adventure Time","She-Ra");
-    $Adult = array("Rick and Morty","Looney Tunes","Daria","Bob's Burgers",
-    "Ninja Turtles","Adventure Time","King of the Hill","Dragon Ball Z");
-    
-    $young = array("Captain Planet","Ninja Turtles","PowerPuff Girls",
-                        "Batman TAS", "Adventure Time", "Daria","Sailer Moon", 
-                        "Dragon Ball Z","The New Adventures of Batman", "G.I. Joe");
-    $middle = array("Bob's Burgers","Batman TAS","Adventure Time", "Daria","Rick and Morty", "King of the Hill",
-                        "Sailer Moon", "Dragon Ball Z","The New Adventures of Batman", "G.I. Joe",
-                        "Looney Tunes","The Flintstones","She-Ra");
-    $old = array("Looney Tunes","King of the Hill","She-Ra","The Flintstones", "G.I. Joe","The New Adventures of Batman");
-    
-    function randomFromMatches($ar1,$ar2,$ar3,$ar4){
+    function imageDisplay($cartoonName){
+        if ($cartoonName=="Captain Planet"){
+            $title="captainPlanet";
+        }else if($cartoonName=="Bob's Burgers"){
+            $title="bobsBurgers";
+        }else if($cartoonName=="Ninja Turtles"){
+            $title="ninjaTurtles";
+        }else if($cartoonName=="PowerPuff Girls"){
+            $title="powerPuffGirls";
+        }else if($cartoonName=="Batman TAS"){
+            $title="batmanTAS";
+        }else if($cartoonName=="Adventure Time"){
+            $title="adventureTime";
+        }else if($cartoonName=="Daria"){
+            $title="daria";
+        }else if($cartoonName=="Rick and Morty"){
+            $title="rickAndMorty";
+        }else if($cartoonName=="King of the Hill"){
+            $title="kingOfTheHill";
+        }else if($cartoonName=="Sailer Moon"){
+            $title="sailerMoon";
+        }else if($cartoonName=="Dragon Ball Z"){
+            $title="dragonBallZ";
+        }else if($cartoonName=="The New Adventures of Batman"){
+            $title="theNewAdventuresOfBatman";
+        }else if($cartoonName=="G.I. Joe"){
+            $title="gIJoe";
+        }else if($cartoonName=="Looney Tunes"){
+            $title="looneyTunes";
+        }else if($cartoonName=="The Flintstones"){
+            $title="theFlintstones";
+        }else if($cartoonName=="She-Ra"){
+            $title="sheRa";
+        }
+        echo "<img src='img/$title.jpg' alt='$title' width='350'/>";
+    }
+    function randomFromMatches($ar1,$ar2,$ar3){
         $matches = array_intersect($ar1,$ar2,$ar3);
+        $matches = array_unique($matches);
         $random = array_rand($matches);
-        return $random;
+        return $matches[$random];
     }
     
-    function mergeGenres($genres){
-        switch (count($genres)) {
-                case 1: $genres=$ar1;
-                        break;
-                case 2: $genres=array_merge($ar1,$ar2);
-                        break;
-                case 3: $genres=array_merge($ar1,$ar2,$ar3);
-                        break;
-                case 4: $genres=array_merge($ar1,$ar2,$ar3,$ar4);
-                        break;
-                }
-                return $genres;
+    function checkAgeRange($input){
+        $young = array("Captain Planet","Ninja Turtles","PowerPuff Girls",
+                        "Batman TAS", "Adventure Time", "Daria","Sailer Moon", 
+                        "Dragon Ball Z","The New Adventures of Batman", "G.I. Joe");
+        $middle = array("Bob's Burgers","Batman TAS","Adventure Time", "Daria",
+                        "Rick and Morty", "King of the Hill", "Sailer Moon", 
+                        "Dragon Ball Z","The New Adventures of Batman", "G.I. Joe",
+                        "Looney Tunes","The Flintstones","She-Ra");
+        $old = array("Looney Tunes","King of the Hill","She-Ra","The Flintstones", 
+                        "G.I. Joe","The New Adventures of Batman");
+        
+        if ($input=="young"){
+            return $young;
+        }else if($input=="middle"){
+            return $middle;
+        }else if($input=="old"){
+            return $old;
+        }
+    }
+    
+    function checkAudience($input){
+        $boys = array("The New Adventures of Batman","Ninja Turtles","G.I. Joe","Looney Tunes","Batman TAS",
+                    "Captain Planet","Dragon Ball Z", "Adventure Time");
+        $girls = array("Sailer Moon","Looney Tunes","PowerPuff Girls", 
+                    "Daria", "Adventure Time","She-Ra");
+        $adults = array("Rick and Morty","Looney Tunes","Daria",
+                        "Bob's Burgers","Ninja Turtles","Adventure Time",
+                        "King of the Hill","Dragon Ball Z");
+        
+        if ($input=="boys"){
+            return $boys;
+        }else if($input=="girls"){
+            return $girls;
+        }else if($input=="adults"){
+            return $adults;
+        }
+    }
+    
+    function checkGenre($array){
+        
+        $campy = array("The New Adventures of Batman","Ninja Turtles",
+                    "G.I. Joe","Sailer Moon","Looney Tunes","The Flintstones",
+                    "She-Ra");
+        $action = array("Captain Planet","Batman TAS","PowerPuff Girls",
+                        "Dragon Ball Z","She-Ra");
+        $comedy = array("Rick and Morty", "Daria","Bob's Burgers","Ninja Turtles",
+                    "Adventure Time","King of the Hill","Looney Tunes",
+                    "The Flintstones");
+        $psa = array("G.I. Joe", "Captain Planet","Looney Tunes","She-Ra");
+        
+        
+        if(in_array("Action", $array)&&in_array("Comedy", $array)&&in_array("Campy", $array)&&in_array("PSA", $array)){
+            $genre = array_merge($action,$comedy,$campy,$psa);
+        }else if(count($array)==3&&!in_array("Action", $array)){
+            $genre = array_merge($psa,$comedy,$campy);
+        }else if((count($array)==3)&&(!in_array("PSA", $array))){
+            $genre = array_merge($action,$comedy,$campy);
+        }else if((count($array)==3)&&(!in_array("Comedy", $array))){
+            $genre = array_merge($action,$psa,$campy);
+        }else if((count($array)==3)&&(!in_array("Campy", $array))){
+            $genre = array_merge($action,$psa,$comedy);
+        }else if(in_array("Campy", $array)&&in_array("Action", $array)){
+            $genre = array_merge($campy,$action);
+        }else if(in_array("Comedy", $array)&&in_array("Action", $array)){
+            $genre = array_merge($comedy,$action);
+        }else if(in_array("PSA", $array)&&in_array("Action", $array)){
+            $genre = array_merge($psa,$action);
+        }else if(in_array("PSA", $array)&&in_array("Action", $array)){
+            $genre = array_merge($psa,$action);
+        }else if(in_array("Campy", $array)&&in_array("Comedy", $array)){
+            $genre = array_merge($campy,$comedy);
+        }else if(in_array("Campy", $array)&&in_array("PSA", $array)){
+            $genre = array_merge($campy,$psa);
+        }else if(in_array("Comedy", $array)&&in_array("PSA", $array)){
+            $genre = array_merge($psa,$comedy);
+        }else if((count($array)==1)&&(in_array("Action", $array))){
+            $genre = $action;
+        }else if((count($array)==1)&&(in_array("Comedy", $array))){
+            $genre = $comedy;
+        }else if((count($array)==1)&&(in_array("PSA", $array))){
+            $genre = $psa;
+        }else if((count($array)==1)&&(in_array("Campy", $array))){
+            $genre = $campy;
+        }
+        $genre = array_unique($genre);
+        return $genre;
     }
     
     
--- hw/hw3/index.php
+++ hw/hw3/index.php
@@ -1,64 +1,168 @@
 <?php
     include 'inc/functions.php';
 
+    // if (isset($_POST['name'])){
+    //   $name = $_POST['name'];
+    //   echo "name: $name";
+      
+    //   if (isset($_POST['ageRange'])){
+    //       $name = $_POST['ageRange'];
+    //       echo "ageRange: $ageRange";
+    //       if ($ageRange=="young"){
+    //           $ageRange = array_merge($young);
+    //       }else if($ageRange=="middle"){
+    //           $ageRange = array_merge($middle);
+    //       }else if($ageRange=="old"){
+    //           $ageRange = array_merge($old);
+    //       }
+    //       print_r($ageRange);
+           
+    //       if (isset($_POST['audience'])){
+    //           $audience = $_POST['audience'];
+    //           echo "audience: $audience";
+    //           if ($audience=="boys"){
+    //               $audience = array_merge($Boys);
+    //           }else if($audience=="girls"){
+    //               $audience = array_merge($Girls);
+    //           }else if($audience=="adults"){
+    //               $audience = array_merge($Adults);
+    //           }
+    //         //   print_r($audience);
+               
+    //           if(isset($_POST['genreChoice'])){
+    //             $genre = $_POST['genreChoice'];
+    //             for ($i=0; $i<count($genre); $i++) {
+    //                 echo "genre:". $genre[$i] ."<br>";
+    //             }
+    //             if(in_array("Action", $genre)&&in_array("Comedy", $genre)&&in_array("Campy", $genre)&&in_array("PSA", $genre)){
+    //                 $genre = array_merge($action,$comedy,$campy,$psa);
+    //             }else if(count($genre)==3&&!in_array("Action", $genre)){
+    //                 $genre = array_merge($psa,$comedy,$campy);
+    //             }else if((count($genre)==3)&&(!in_array("PSA", $genre))){
+    //                 $genre = array_merge($action,$comedy,$campy);
+    //             }else if((count($genre)==3)&&(!in_array("Comedy", $genre))){
+    //                 $genre = array_merge($action,$psa,$campy);
+    //             }else if((count($genre)==3)&&(!in_array("Campy", $genre))){
+    //                 $genre = array_merge($action,$psa,$comedy);
+    //             }else if(in_array("Campy", $genre)&&in_array("Action", $genre)){
+    //                 $genre = array_merge($campy,$action);
+    //             }else if(in_array("Comedy", $genre)&&in_array("Action", $genre)){
+    //                 $genre = array_merge($comedy,$action);
+    //             }else if(in_array("PSA", $genre)&&in_array("Action", $genre)){
+    //                 $genre = array_merge($psa,$action);
+    //             }else if(in_array("PSA", $genre)&&in_array("Action", $genre)){
+    //                 $genre = array_merge($psa,$action);
+    //             }else if(in_array("Campy", $genre)&&in_array("Comedy", $genre)){
+    //                 $genre = array_merge($campy,$comedy);
+    //             }else if(in_array("Campy", $genre)&&in_array("PSA", $genre)){
+    //                 $genre = array_merge($campy,$psa);
+    //             }else if(in_array("Comedy", $genre)&&in_array("PSA", $genre)){
+    //                 $genre = array_merge($psa,$comedy);
+    //             }else if((count($genre)==1)&&(in_array("Action", $genre))){
+    //                 $genre = $action;
+    //             }else if((count($genre)==1)&&(in_array("Comedy", $genre))){
+    //                 $genre = $comedy;
+    //             }else if((count($genre)==1)&&(in_array("PSA", $genre))){
+    //                 $genre = $psa;
+    //             }else{
+    //                 $genre = $campy;
+    //             }
+    //             $genre = array_unique($genre);
+    //             // print_r($genre);
+    //           }
+    //       }
+    //   }
+    //   randomFromMatches($genre,$ar2,$ar3);
+    // }
+
+    // if (isset($_POST['name'])){
+    //     $name = $_POST['name'];
+    //     echo "name: $name";
+    //     echo "<br>";
+    // }
+    // if (isset($_POST['ageRange'])){
+    //     $ageRange = $_POST['ageRange'];
+    //     echo "ageRange: $ageRange";
+    //     echo "<br>";
+    // }
+    
+    // if (isset($_POST['audience'])){
+    // $audience = $_POST['audience'];
+    // echo "audience: $audience";
+    // echo "<br>";
+    // }
+    
+    // if(isset($_POST['genreChoice'])){
+    //     $genre = $_POST['genreChoice'];
+    //     for ($i=0; $i<count($genre); $i++) {
+    //         echo "genre:". $genre[$i] ."<br>";
+    //     }
+    //     if(in_array("Action", $genre)&&in_array("Comedy", $genre)&&in_array("Campy", $genre)&&in_array("PSA", $genre)){
+    //         $genre = array_merge($action,$comedy,$campy,$psa);
+    //     }else if(count($genre)==3&&!in_array("Action", $genre)){
+    //         $genre = array_merge($psa,$comedy,$campy);
+    //     }else if((count($genre)==3)&&(!in_array("PSA", $genre))){
+    //         $genre = array_merge($action,$comedy,$campy);
+    //     }else if((count($genre)==3)&&(!in_array("Comedy", $genre))){
+    //         $genre = array_merge($action,$psa,$campy);
+    //     }else if((count($genre)==3)&&(!in_array("Campy", $genre))){
+    //         $genre = array_merge($action,$psa,$comedy);
+    //     }else if(in_array("Campy", $genre)&&in_array("Action", $genre)){
+    //         $genre = array_merge($campy,$action);
+    //     }else if(in_array("Comedy", $genre)&&in_array("Action", $genre)){
+    //         $genre = array_merge($comedy,$action);
+    //     }else if(in_array("PSA", $genre)&&in_array("Action", $genre)){
+    //         $genre = array_merge($psa,$action);
+    //     }else if(in_array("PSA", $genre)&&in_array("Action", $genre)){
+    //         $genre = array_merge($psa,$action);
+    //     }else if(in_array("Campy", $genre)&&in_array("Comedy", $genre)){
+    //         $genre = array_merge($campy,$comedy);
+    //     }else if(in_array("Campy", $genre)&&in_array("PSA", $genre)){
+    //         $genre = array_merge($campy,$psa);
+    //     }else if(in_array("Comedy", $genre)&&in_array("PSA", $genre)){
+    //         $genre = array_merge($psa,$comedy);
+    //     }else if((count($genre)==1)&&(in_array("Action", $genre))){
+    //         $genre = $action;
+    //     }else if((count($genre)==1)&&(in_array("Comedy", $genre))){
+    //         $genre = $comedy;
+    //     }else if((count($genre)==1)&&(in_array("PSA", $genre))){
+    //         $genre = $psa;
+    //     }else{
+    //         $genre = $campy;
+    //     }
+    //     $genre = array_unique($genre);
+    //     print_r($genre);
+    // }
     if (isset($_POST['name'])){
         $name = $_POST['name'];
-        echo "name: $name";
-        echo "<br>";
     }
+    
     if (isset($_POST['ageRange'])){
         $ageRange = $_POST['ageRange'];
-        echo "ageRange: $ageRange";
-        echo "<br>";
+        $ageArray = checkAgeRange($ageRange);
+        // print_r($ageArray);
+        // echo "<br>";
+    
+        if (isset($_POST['audience'])){
+            $audience = $_POST['audience'];
+            $audienceArray = checkAudience($audience);
+            // print_r($audienceArray);
+            // echo "<br>";
+            
+            if(isset($_POST['genreChoice'])){
+                $genre = $_POST['genreChoice'];
+                $genreArray=checkGenre($genre);
+                // print_r($genreArray);
+                // echo "<br>";
+                
+                $matches = array_intersect($genreArray,$audienceArray,$ageArray);
+                $matches = array_unique($matches);  //creates array of unique matches from previous arrays
+                $random = array_rand($matches);     //returns random key from matches array
+                $cartoonName = $matches[$random];          //name of cartoon match from matches array
+            }
+        }
     }
     
-    if(isset($_POST['genreChoice'])){
-        $genre = $_POST['genreChoice'];
-        for ($i=0; $i<count($genre); $i++) {
-            echo "genre:". $genre[$i] ."<br>";
-        }
-        if(in_array("Action", $genre)&&in_array("Comedy", $genre)&&in_array("Campy", $genre)&&in_array("PSA", $genre)){
-            $genre = array_merge($action,$comedy,$campy,$psa);
-        }else if(count($genre)==3&&!in_array("Action", $genre)){
-            $genre = array_merge($psa,$comedy,$campy);
-        }else if((count($genre)==3)&&(!in_array("PSA", $genre))){
-            $genre = array_merge($action,$comedy,$campy);
-        }else if((count($genre)==3)&&(!in_array("Comedy", $genre))){
-            $genre = array_merge($action,$psa,$campy);
-        }else if((count($genre)==3)&&(!in_array("Campy", $genre))){
-            $genre = array_merge($action,$psa,$comedy);
-        }else if(in_array("Campy", $genre)&&in_array("Action", $genre)){
-            $genre = array_merge($campy,$action);
-        }else if(in_array("Comedy", $genre)&&in_array("Action", $genre)){
-            $genre = array_merge($comedy,$action);
-        }else if(in_array("PSA", $genre)&&in_array("Action", $genre)){
-            $genre = array_merge($psa,$action);
-        }else if(in_array("PSA", $genre)&&in_array("Action", $genre)){
-            $genre = array_merge($psa,$action);
-        }else if(in_array("Campy", $genre)&&in_array("Comedy", $genre)){
-            $genre = array_merge($campy,$comedy);
-        }else if(in_array("Campy", $genre)&&in_array("PSA", $genre)){
-            $genre = array_merge($campy,$psa);
-        }else if(in_array("Comedy", $genre)&&in_array("PSA", $genre)){
-            $genre = array_merge($psa,$comedy);
-        }else if((count($genre)==1)&&(in_array("Action", $genre))){
-            $genre = $action;
-        }else if((count($genre)==1)&&(in_array("Comedy", $genre))){
-            $genre = $comedy;
-        }else if((count($genre)==1)&&(in_array("PSA", $genre))){
-            $genre = $psa;
-        }else{
-            $genre = $campy;
-        }
-        array_unique($genre);
-        print_r($genre);
-    }
-    
-    if (isset($_POST['audience'])){
-        $audience = $_POST['audience'];
-        echo "audience: $audience";
-        echo "<br>";
-    }       
     
 
 ?>
@@ -77,14 +181,44 @@
 
 </head>
 
-<body">
-    <h2 id="title">What Saturday Morning Cartoon Should You Watch?</h2>
+<body>
+    <?php
+    if (!isset($matches)){
+        echo "Please complete all forms!";
+    }else if(empty($name)){
+        echo "Enter your name.";
+    }else if(empty($ageRange)){
+        echo "Enter choose genre(s).";
+    }else if(empty($audience)){
+        echo "Please choose age range.";
+    }else if(empty($genre)){
+        echo "Please choose a target audience.";
+    }
+
+    ?>
+    
+    
+    <div id="titles">
+    <h2 id="title">What Saturday Morning Cartoon Should You Watch?<br></h2>
+    <b id="subtitle">Find a random show based on your interests!</b>
+    </div>
     <form method="post">
+    <?php
+        if (isset($cartoonName)){
+            echo "<div id='cartoonName'>";
+            echo "<h4> Hey $name, based on your input, you'll really enjoy $cartoonName!</h4>";
+            echo "<br>";
+            imageDisplay($cartoonName);
+            echo "</div>";
+        }
+    ?>
+    
+    
     
         <div id="nameDiv">
             <b><u>What's your name?</u></b>
             <br><br>
-            <input type="text" name="name" placeholder="Enter Name">
+            <input value="<?php echo htmlspecialchars($_POST['name']); ?>" type="text" name="name" placeholder="Enter Name">
             <br>
         </div>
         
@@ -92,13 +226,13 @@
             <b><u>What Age Range Are You In?</u></b>
             <br><br>
             
-            <input type="radio" id="age" name="ageRange"  value="young">
+            <input <?php if (isset($ageRange) && $ageRange=="young") echo "checked"; ?> type="radio" id="age" name="ageRange"  value="young">
             <label for="young">2-16</label>
             
-            <input type="radio" id="age" name="ageRange"  value="middle">
+            <input <?php if (isset($ageRange) && $ageRange=="middle") echo "checked"; ?> type="radio" id="age" name="ageRange"  value="middle">
             <label for="middle">17-49</label>
             
-            <input type="radio" id="age" name="ageRange" value="old">
+            <input <?php if (isset($ageRange) && $ageRange=="old") echo "checked"; ?> type="radio" id="age" name="ageRange" value="old">
             <label for="old">49 and up</label>
 
         </div>
@@ -106,16 +240,16 @@
         <div id="genreDiv">
             <div id="genreTitle"><b><u>Choose A Genre</u></b></div>
             <br><br>
-            <input type="checkbox" id="genre" name="genreChoice[]" value = "Action">
+            <input type="checkbox" <?php if(isset($_POST['genreChoice'])&&in_array("Action",$genre)) echo "checked='checked'"; ?> id="genre" name="genreChoice[]" value = "Action">
             <label for="Action">Action-adventure</label>
             <br>
-            <input type="checkbox" id="genre" name="genreChoice[]" value ="Comedy">
+            <input type="checkbox" <?php if(isset($_POST['genreChoice'])&&in_array("Comedy",$genre)) echo "checked='checked'"; ?> id="genre" name="genreChoice[]" value ="Comedy">
             <label for="Comedy">Comedy</label>
             <br>
-            <input type="checkbox" id="genre" name="genreChoice[]" value ="PSA">
+            <input type="checkbox" <?php if(isset($_POST['genreChoice'])&&in_array("PSA",$genre)) echo "checked='checked'"; ?> id="genre" name="genreChoice[]" value ="PSA">
             <label for="PSA">Public Service Announcement</label>
             <br>
-            <input type="checkbox" id="genre" name="genreChoice[]" value ="Campy">
+            <input type="checkbox" <?php if(isset($_POST['genreChoice'])&&in_array("Campy",$genre)) echo "checked='checked'"; ?> id="genre" name="genreChoice[]" value ="Campy">
             <label for="Campy">Campy</label>
         </div>
         
@@ -124,9 +258,9 @@
         <br><br>
             <select name="audience">
                 <option value="">Select One</option>
-                <option value="boys" name="audience">Boys</option>
-                <option value="girls" name="audience">Girls</option>
-                <option value="adults" name="audience">Adults</option>
+                <option <?php if($audience == 'boys') { ?> selected <?php } ?> value="boys" name="audience">Boys</option>
+                <option <?php if($audience == 'girls') { ?> selected <?php } ?> value="girls" name="audience">Girls</option>
+                <option <?php if($audience == 'adults') { ?> selected <?php } ?> value="adults" name="audience">Adults</option>
             </select>
             
         </div>
